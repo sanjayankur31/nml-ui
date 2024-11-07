646ca8ef54bb4ec90eabc71596c931b1ebf25ba9
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2024-11-07 18:04:19 +0000

    fix: correct metadata and add entry point

--- nml_ui/main.py
+++ nml_ui/main.py
@@ -8,3 +8,16 @@ Copyright 2024 Ankur Sinha
 Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
 """
 
+
+from tkinter import Tk
+from tkinter import ttk
+
+
+
+def app():
+    """Main app method."""
+    nmlgui = Tk()
+    nmlgui.title("NeuroML GUI")
+
+    mainframe = ttk.Frame(nmlgui)
+    nmlgui.mainloop()

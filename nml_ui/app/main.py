a231351a0b55d745317af687356ed0b696e2e21e
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2024-11-07 21:55:23 +0000

    feat: add menus to create new object and load a NML model

---
+++ nml_ui/app/main.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+Enter one line description here.
+
+File:
+
+Copyright 2024 Ankur Sinha
+Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
+"""
+
+
+import sys
+import logging
+from neuroml.utils import component_factory
+from pyneuroml.io import read_neuroml2_file
+from tkinter import filedialog
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+
+
+def file_new_callback(app):
+    """Create a new neuroml model object
+    :returns: TODO
+
+    """
+    if app.model is not None:
+        print("Document will be overwritten")
+        # TODO: add a confirmation dialog
+
+    app.model = component_factory("NeuroMLDocument", id="new_doc")
+    # logger.info("New doc created")
+    print("New doc created")
+
+
+def file_load_callback(app):
+    """Load a neuroml file
+    :returns: TODO
+
+    """
+    filename = filedialog.askopenfilename(title="Select a NeuroML file",
+                                          filetypes=[("NeuroML2", "*.nml")])
+    if filename:
+        if app.model is not None:
+            print("Document will be overwritten")
+            # TODO: add a confirmation dialog
+        app.model = read_neuroml2_file(filename)
+        app.filename = filename
+        app.nmlgui.title(f"NeuroML GUI - {filename}")
+
+    print(f"File loaded: {app.model}")
+
+
+def file_exit_callback(app):
+    """Exit the app
+    :returns: TODO
+
+    """
+    if app.model is not None:
+        print("Save before exit")
+        # TODO: add a confirmation dialog
+
+    sys.exit(0)
--- nml_ui/main.py
+++ nml_ui/main.py
@@ -9,15 +9,58 @@ Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
 """
 
 
-from tkinter import Tk
+from tkinter import Tk, Menu
 from tkinter import ttk
+from nml_ui.app.main import file_new_callback, file_load_callback, file_exit_callback
+from neuroml.utils import component_factory
 
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+class NeuroMLGUI(object):
+
+    """Main NeuroML GUI class"""
+
+    def __init__(self):
+        """Basic initialization"""
+        # place holder object model
+        self.model = component_factory("NeuroMLDocument", id="new_doc")
+        self.filename = ""
+
+        self.nmlgui = Tk()
+        self.nmlgui.title("NeuroML GUI")
+        self.nmlgui.option_add("*tearOff", False)
+
+    def setup(self):
+        """Setup the app"""
+        # menu
+        self.menubar = Menu(self.nmlgui)
+
+        # file menu
+        self.menu_file = Menu(self.menubar)
+        self.menu_file.add_command(label="New", command=lambda: file_new_callback(self))
+        self.menu_file.add_command(label="Load", command=lambda: file_load_callback(self))
+        self.menu_file.add_command(label="Exit", command=lambda: file_exit_callback(self))
+
+        self.menubar.add_cascade(menu=self.menu_file, label="File")
+        self.nmlgui['menu'] = self.menubar
+
+        # mainframe = ttk.Frame(nmlgui)
+
+    def run(self):
+        """Run the app.
+        :returns: TODO
+
+        """
+        self.setup()
+        self.nmlgui.mainloop()
 
 
 def app():
-    """Main app method."""
-    nmlgui = Tk()
-    nmlgui.title("NeuroML GUI")
-
-    mainframe = ttk.Frame(nmlgui)
-    nmlgui.mainloop()
+    """Runner function for script end point"""
+    app = NeuroMLGUI()
+    app.run()
